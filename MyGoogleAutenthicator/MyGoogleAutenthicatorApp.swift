37839388610664c8ae3b37011df280b28b08b46d
lione95 <113521895+lione95@users.noreply.github.com>
2022-11-15 15:38:21 +0100

    Initial Commit

---
+++ MyGoogleAutenthicator/MyGoogleAutenthicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGoogleAutenthicatorApp.swift
+//  MyGoogleAutenthicator
+//
+//  Created by Mattia Golino on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MyGoogleAutenthicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
